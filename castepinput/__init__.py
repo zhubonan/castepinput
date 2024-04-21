8ae42be7ec3d5486930daad71b321b392cc89aa3
Bonan Zhu <zhubonan@outlook.com>
2024-04-21 17:20:09 +0800

    Bump version

--- castepinput/__init__.py
+++ castepinput/__init__.py
@@ -4,5 +4,5 @@ Base module of castepinput
 from .inputs import ParamInput, CellInput
 from .common import Block
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 __all__ = ["Block", "ParamInput", "CellInput"]
